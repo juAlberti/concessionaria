1fa76d13d3ea28ce1a35157d2ae97b2a0a589e20
juAlberti <julia.maia@acad.pucrs.br>
2021-03-16 21:56:58 +0000

    Esqueleto

---
+++ Veiculo.java
@@ -0,0 +1,9 @@
+package concessionaria;
+
+public interface Veiculo {
+    String placa;
+    String marca;
+    String modelo;
+    int ano;
+    double valor;
+}
---
+++ VeiculoUtilitario.java
@@ -0,0 +1,6 @@
+package concessionaria;
+
+public class VeiculoUtilitario implements Veiculo{
+    int capCargaTon;
+    int nroEixos;
+}
